2ddc88758ede7cb76fb5c40a3404ac1f0d866995
jonatha-mccrary <jonathan.mccrary@me.com>
2020-08-15 15:24:47 -0500

    Initial Commit

---
+++ RouteWare_LocationSearch/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RouteWare_LocationSearch.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
