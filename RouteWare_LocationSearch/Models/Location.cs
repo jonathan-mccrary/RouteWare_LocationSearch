00d28e20bf1b0463f264a1918954dda48f102509
jonatha-mccrary <jonathan.mccrary@me.com>
2020-08-15 15:46:56 -0500

    * Data.csv: * Location.cs:
    * RouteWare_LocationSearch.csproj: Add Location model

---
+++ RouteWare_LocationSearch/Models/Location.cs
@@ -0,0 +1,27 @@
+﻿
+using GeoCoordinatePortable;
+
+namespace RouteWare_LocationSearch.Models
+{
+    public class Location
+    {
+        public Location()
+        {
+
+        }
+
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public double Latitude { get; set; }
+        public double Longitudue { get; set; }
+        public GeoCoordinate Coordinate
+        {
+            get
+            {
+                return new GeoCoordinate(Latitude, Longitudue);
+            }
+        }
+    }
+}
