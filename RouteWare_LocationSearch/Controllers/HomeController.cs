0bf47dcee3d861b9fd26c41e76a3a85197a05bad
jonatha-mccrary <jonathan.mccrary@me.com>
2020-08-15 23:00:31 -0500

    Update to Controller

--- RouteWare_LocationSearch/Controllers/HomeController.cs
+++ RouteWare_LocationSearch/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using RouteWare_LocationSearch.Contracts;
 using RouteWare_LocationSearch.Models;
 
 namespace RouteWare_LocationSearch.Controllers
@@ -12,10 +13,12 @@ namespace RouteWare_LocationSearch.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ICSVReader _csvReader;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ICSVReader csvReader)
         {
             _logger = logger;
+            _csvReader = csvReader;
         }
 
         public IActionResult Index()
@@ -30,6 +33,18 @@ namespace RouteWare_LocationSearch.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public ActionResult Search(string searchString)
+        {
+            var test = _csvReader.GetLocationsBySearch(searchString);
+            return View(test);
+        }
+
+        public ActionResult GetNearbyLocations(Location location)
+        {
+            var test = _csvReader.GetLocationsByCoordinate(location);
+            return View(test);
         }
     }
 }
